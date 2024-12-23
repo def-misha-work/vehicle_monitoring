78ceb445bbc98dba68dcaf56274f542c0facdd51
BaronFAS <mikhail-upside@yandex.ru>
2024-12-23 20:13:08 +0300

    Fix login

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,6 @@
+import requests
+import logging
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import (
     authenticate,
@@ -7,6 +10,13 @@ from django.contrib.auth import (
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from cars.models import Car
+from cars.constants import (
+    URL_LOGIN,
+    USER_LOGIN,
+    USER_PASSWORD,
+)
+
+logging.basicConfig(level=logging.INFO)
 
 
 @login_required
@@ -22,10 +32,39 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            auth_login(request, user)
-            return redirect('car_list')
+        # проверяем есть ли юзер в базе клиента
+        URL_GET_ENTER = (
+            f"{URL_LOGIN}?account={USER_LOGIN}&password={USER_PASSWORD}"
+        )
+        response = requests.get(URL_GET_ENTER)
+        try:
+            response.raise_for_status()
+            data = response.json()
+        except requests.exceptions.HTTPError as e:
+            logging.info(f"Ошибка запроса к api {e}")
+            return render(
+                request, "cars/login.html",
+                {"error": "Неверный логин или пароль"}
+            )
+        # TODO тут надо сделать запрос к базе данных
+        # на предмет наличия "jsession"
+        if "jsession" not in data:
+            logging.info("Ключ 'jsession' отсутствует в ответе")
+            return render(
+                request, "cars/login.html",
+                {"error": "Неверный логин или пароль"}
+            )
+        if data["jsession"] == "817328fdd0274ce283ec715b215d5b4c":
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return redirect('car_list')
+            else:
+                return render(
+                    request, "cars/login.html",
+                    {"error": "Неверный логин или пароль"}
+                )
+        # TODO убрать после норм авторизации
         else:
             return render(
                 request, "cars/login.html",
---
+++ req.py
@@ -0,0 +1,15 @@
+﻿import requests
+
+
+URL_LOGIN = "http://s1.mdvr.kz:88/StandardApiAction_login.action"
+ACCOUNT = "?account="
+PASSWORD = "&password="
+USER_LOGIN = "test"
+USER_PASSWORD = "181023"
+URL_GET_ENTER = URL_LOGIN + ACCOUNT + USER_LOGIN + PASSWORD + USER_PASSWORD
+
+
+response = requests.get(URL_GET_ENTER)
+data = response.json()
+print(response.status_code)
+print(data["jsession"])
