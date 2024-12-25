6ee10b49f459b605bad42ef9075208272822247a
BaronFAS <mikhail-upside@yandex.ru>
2024-12-25 22:24:31 +0300

    Add const

--- cars/constants.py
+++ cars/constants.py
@@ -1,5 +1,16 @@
-﻿URL_LOGIN = "http://s1.mdvr.kz:88/StandardApiAction_login.action"
+﻿DOMEN = "http://s1.mdvr.kz:88/"
+ACCOUNT = "?account="
+PASSWORD = "&password="
+JSESSION = "?jsession="
+DEVIDNO = "&devIdno="
+BEGINTIME = "&begintime="
+ENDTIME = "&endtime="
 
+URL_GET_JSESSION = DOMEN + "StandardApiAction_login.action"
+URL_GET_TECH = DOMEN + "StandardApiAction_queryUserVehicle.action"
+URL_GET_WEIGHT = DOMEN + "StandardApiAction_getDeviceStatus.action"
+
+# 2024-12-22%2000:00:00
 # USER_LOGIN = "test"
 # USER_PASSWORD = "181023"
 # USER_PASSWORD = "222"
