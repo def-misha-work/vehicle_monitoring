00d0cc337f0ed012307de4390a4cf66c96846634
BaronFAS <mikhail-upside@yandex.ru>
2024-12-30 18:37:21 +0300

    Fix

--- cars/views.py
+++ cars/views.py
@@ -27,10 +27,12 @@ def car_list(request):
         jsession_obj = Jsession.objects.get(user=user)
         jsession = jsession_obj.jsession
     except Jsession.DoesNotExist:
-        return render(
-                request, "cars/login.html",
-                {"error": "Ошибка авторизации"}
-            )
+        # TODO доделать редирект с тестом
+        return redirect('login')
+        # return render(
+        #         request, "cars/login.html",
+        #         {"error": "Ошибка авторизации"}
+        #     )
 
     # Получаем технику
     result = get_tech(jsession)
@@ -73,41 +75,43 @@ def car_list(request):
 
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        # проверяем есть ли юзер в базе клиента
-        url_get_jsession = (
-            f"{URL_GET_JSESSION}?account={username}&password={password}"
+    if request.method != 'POST':
+        return render(request, "cars/login.html")
+    username = request.POST['username']
+    password = request.POST['password']
+    # проверяем есть ли юзер в базе клиента
+    url_get_jsession = (
+        f"{URL_GET_JSESSION}?account={username}&password={password}"
+    )
+    response = requests.get(url_get_jsession)
+    if response.status_code != 200:
+        logging.warning("Ответ не 200")
+        return render(
+            request, "cars/login.html",
+            {"error": "Ошибка авторизации"}
         )
-        try:
-            response = requests.get(url_get_jsession)
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            logging.info(f"Ошибка запроса к api {e}")
-            return render(
-                request, "cars/login.html",
-                {"error": "Неверный логин или пароль"}
-            )
-
-        data = response.json()
-        if "jsession" not in data:
-            logging.info("Ключ 'jsession' отсутствует в ответе")
-            return render(
-                request, "cars/login.html",
-                {"error": "Неверный логин или пароль"}
-            )
-        # Создаем или получаем пользователя
-        user, _ = User.objects.get_or_create(username=username)
-        # Создаем или обновляем Jsession, связывая его с пользователем
-        Jsession.objects.update_or_create(
-            user=user,  # Ищем запись по пользователю
-            defaults={'jsession': data["jsession"]}  # Обновляем поле jsession
+    if "message" in response.text:
+        logging.warning(f"Ошибка api {response.text}")
+        return render(
+            request, "cars/login.html",
+            {"error": "Ошибка авторизации"}
         )
-        auth_login(request, user)
-        return redirect('car_list')
-
-    return render(request, "cars/login.html")
+    data = response.json()
+    if "jsession" not in data:
+        logging.info("Ключ 'jsession' отсутствует в ответе")
+        return render(
+            request, "cars/login.html",
+            {"error": "Неверный логин или пароль"}
+        )
+    # Создаем или получаем пользователя
+    user, _ = User.objects.get_or_create(username=username)
+    # Создаем или обновляем Jsession, связывая его с пользователем
+    Jsession.objects.update_or_create(
+        user=user,  # Ищем запись по пользователю
+        defaults={'jsession': data["jsession"]}  # Обновляем поле jsession
+    )
+    auth_login(request, user)
+    return redirect('car_list')
 
 
 def logout_view(request):
--- req.py
+++ req.py
@@ -5,11 +5,13 @@ URL_LOGIN = "http://s1.mdvr.kz:88/StandardApiAction_login.action"
 ACCOUNT = "?account="
 PASSWORD = "&password="
 USER_LOGIN = "test"
-USER_PASSWORD = "181023"
+# USER_PASSWORD = "181023"
+USER_PASSWORD = "181024"
 URL_GET_ENTER = URL_LOGIN + ACCOUNT + USER_LOGIN + PASSWORD + USER_PASSWORD
 
 
 response = requests.get(URL_GET_ENTER)
 data = response.json()
 print(response.status_code)
-print(data["jsession"])
+print(response.text)
+# print(data["jsession"])
